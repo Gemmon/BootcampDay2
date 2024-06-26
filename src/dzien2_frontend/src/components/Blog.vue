4642e65e24e2e656f9558af654cc1cc501f08120
Gemmon <kchrapowicz@gmail.com>
2024-06-26 19:06:23 +0200

    changed interfaces

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
         <h2 class="text-blue-600"> Wpisy na bloga</h2>
-        <button @click="pobierzWpisy">refresh</button>
-        <div v-for="wpis in wpisy">
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded-sm text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+        <div v-for="wpis in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
             <p> {{ wpis }}</p>
             </div>
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
-    </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" class="border-2 vorder-blue-600 p-4" type="text">
+            <button class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+            </div>
+        </div>
 </template>
 
 <script>
